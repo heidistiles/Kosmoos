70ef09b90e61eff73712a300338fa75dba3c9269
Heidi S <stile080@umn.edu>
2024-12-09 15:33:28 -0600

    Updating schematic info

--- src/data_sources/ArtifactSchematics.js
+++ src/data_sources/ArtifactSchematics.js
@@ -1,10 +1,58 @@
 const data = [
     {
-        "id": "",
-        "artifact_name": "Mercury",
+        "id": "gravity-speeder",
+        "artifact_name": "Gravity Speeder",
         "artifact_gif": "Urrey",
         "planet_symbol": "",
         "description": "surrey0@cafepress.com"
     },
-
+    {
+        "id": "hyno-thesia",
+        "artifact_name": "Hypno-Thesia",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "wormhole-compass",
+        "artifact_name": "Wormhole Compass",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "ferric-corroder",
+        "artifact_name": "Ferric Corroder",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "stun-blaster",
+        "artifact_name": "Stun Blaster",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "spacetime-warper",
+        "artifact_name": "Spacetime Warper",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "astral-rotor",
+        "artifact_name": "Astral Rotor",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
+    {
+        "id": "grappling-trident",
+        "artifact_name": "Grappling Trident",
+        "artifact_gif": "Urrey",
+        "planet_symbol": "",
+        "description": "surrey0@cafepress.com"
+    },
 ]
